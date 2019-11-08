03ea2bb8465a83bd13989c11ec71b49469e96ab4
Scare983 <kevinscare983@gmail.com>
2019-11-08 16:27:13 -0500

    argv update

--- retrieveData.py
+++ retrieveData.py
@@ -2,78 +2,63 @@ import getopt
 import sys
 import numpy as np
 import pandas as pd
-
+import bs4
+argumentHash = {'ch4':False, 'weather': False, 'sf6':False, 'debug':False}
 def prossargs():
     try:
-        opt, args = getopt.getopt(sys.argv[1:], "")
-    except getopt.GetoptError:
-        usage()def prossargs():
-    try:
-        opt, args = getopt.getopt(sys.argv[1:], "s:fk:")
+        opt, args = getopt.getopt(sys.argv[1:], "cwnshd")
     except getopt.GetoptError:
         usage()
-        sys.exit(2)
     for opts, arg in opt:
-        if opts == '-s':
-            if not arg.isdigit():
-                usage()
-            elif int(arg) <=0:
-                print('Cannot have simtime <= 0, exiting...')
-            else:
-                argumentHash['SIM_TIME'] = int(arg)
-        elif opts == '-f':
-            argumentHash['isFixedLatency'] = True
-        elif opts == '-k':
-            if not arg.isdigit():
-                usage()
-            elif int(arg) <=0:
-                print('Cannot have K <= 0, exiting...')
-            else:
-                argumentHash['maxLatency'] = int(arg)
+        if opts == '-h':
+            help()
+        elif opts == '-c':
+            argumentHash['ch4'] = True
+        elif opts == '-w':
+            argumentHash['weather'] = True
+        elif opts == '-s':
+            argumentHash['sf6'] = True
+        elif opts == 'd':
+            argumentHash['debug'] = True
+        else:
+            help()
+    flag = False
     for a in argumentHash.keys():
-        if argumentHash[a] is None:
-            usage()
+        if argumentHash[a] == True:
+            flag = False
+            break
+        else:
 
+            flag = True
+    if flag:
+        print('No arguments turned on.')
+        help()
 
 
 def help():
     print('This file is used to update or retrieve infromation from websites ')
     print('the data found is stored inside a ./data folder and goes to corresponding ')
     print('folders for the desired atmospheric condition we are grabbing.')
+    usage()
+
 def usage():
     print('please input correct flags to update data files ')
-    print('C = CH4, w = weather, n=n02 s=sf6 h=help')
+    print('c = CH4, w = weather, n=n02 s=sf6 h=help d=debugMode')
     print(
-        "python retrieveData [-cwnsh] ")
+        "USAGE:  python retrieveData.py [-cwnshd]")
     exit(2)
 
-
-    sys.exit(2)
-    for opts, arg in opt:
-        if opts == '-s':
-            if not arg.isdigit():
-                usage()
-            elif int(arg) <=0:
-                print('Cannot have simtime <= 0, exiting...')
-            else:
-                argumentHash['SIM_TIME'] = int(arg)
-        elif opts == '-f':
-            argumentHash['isFixedLatency'] = True
-        elif opts == '-k':
-            if not arg.isdigit():
-                usage()
-            elif int(arg) <=0:
-                print('Cannot have K <= 0, exiting...')
-            else:
-                argumentHash['maxLatency'] = int(arg)
-    for a in argumentHash.keys():
-        if argumentHash[a] is None:
-            usage()
+def debug(statement):
+    if argumentHash['debug']:
+        print(statement)
 
 
-def usage():
-    print('please input only int values for args')
-    print(
-        "python ping.py -s 'INT-TIME' [-f optional ] -k 'INT -upper Latency'")
-    exit(2)
 
+
+
+
+
+
+
+### main ###
+prossargs()
