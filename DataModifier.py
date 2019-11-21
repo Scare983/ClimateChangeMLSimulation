6926f47b81a6c19a23a0881dd2d037cfe5c3b34a
mymc <catch2123@gmail.com>
2019-11-21 01:30:26 -0500

    added a year to day function for the data modifier, but still requires conversion to weeks and months

--- DataModifier.py
+++ DataModifier.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import glob
 from calendar import monthrange
-import datetime
+import datetime as dt
 import numpy as np
 
 class DateMod():
@@ -9,14 +9,35 @@ class DateMod():
     weekDataFrame = None
     dayDataFrame = None
     def __init__(self, df, colValName):
-        self.month_to_days(df,colValName)
+        if "month" in df.keys():
+            self.month_to_days(df,colValName)
+        elif 'year' in df.keys():
+            self.year_to_days(df,colValName)
         #TODO: fill in nan values created from months_to_day (They will = 0) to predicted ones.
     # testing on ch4, go up to 2014
     #returns dataframe with datetime as index, and values for each day given
+    def year_to_days(self,df, colValName):
+        dateObjs = []
+        dateVals = []
+        for i,y in enumerate(df.year):
+            start = dt.date(int(y), 1, 1)
+            end = dt.date(int(y)+1,1, 1)
+            date = start
+            days = (end - start).days
+            value = int(df[colValName][i])
+            while date != end:
+                dateObjs.append(date)
+                dateVals.append(value/days)
+                date += dt.timedelta(days=1)
+                df = pd.DataFrame(dateVals, index=dateObjs, columns=[colValName])
+        self.dayDataFrame = df
+       # self.monthDataFrame = pd.DataFrame(df[colValName].resample('M').sum(), columns=[colValName])
+       # self.weekDataFrame = pd.DataFrame(df[colValName].resample('W').sum(), columns=[colValName])
     def month_to_days(self, df, colValName):
         dateObjs = []
         dateVals = []
         if int(df.year[0]) > 1960:
+
             diff = int(df.year[0]) - 1960
             # find delta, and append to month range list
 
@@ -26,10 +47,8 @@ class DateMod():
                     if 1960+year == int(df.year[0]) and int(df.month[0]) == month:
                         break
                     for days in range(1, (monthrange(int(df.year[0]) + year, month)[1])):
-                        dateObjs.append(datetime.datetime(1960+year, month, days))
+                        dateObjs.append(dt.datetime(1960+year, month, days))
                         dateVals.append(np.nan)
-        elif df.year[0] != 1960:
-            raise Exception('the year given has not been accounted for')
         # add known dates to monthrange
         for index, row in df.iterrows():
             # check if row and month exist in delta time, if it doesn't add it.
@@ -37,7 +56,7 @@ class DateMod():
             numDaysInMonth = (monthrange(int(row['year']), int(row['month']))[1])
             valueToAppendToDay = int(row[colValName])/numDaysInMonth
             for days in range(1, numDaysInMonth+1):
-                dateObjs.append(datetime.datetime(int(row['year']), int(row['month']), days))
+                dateObjs.append(dt.datetime(int(row['year']), int(row['month']), days))
                 dateVals.append(valueToAppendToDay)
 
         # TODO HERE:  add dates after the end date of the given df to equal 2019.... maybe..
